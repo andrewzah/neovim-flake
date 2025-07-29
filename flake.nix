f5e38c0652688010ecb61767a7cd81c22e6393b8
Andrew Zah <zah@andrewzah.com>
2025-07-30 01:54:06 +0900

    support default systems

--- flake.nix
+++ flake.nix
@@ -1,36 +1,41 @@
 {
   description = "nvf neovim flake";
 
-  outputs = {nixpkgs, ...} @ inputs: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
 
-    nvim =
-      (inputs.nvf.lib.neovimConfiguration {
-        inherit pkgs;
-        modules = [
-          ./modules/default.nix
-        ];
-      }).neovim;
-  in {
-    apps.x86_64-linux.default = {
-      type = "app";
-      program = "${nvim}/bin/nvim";
-    };
-
-    packages.x86_64-linux.default = nvim;
-
-    devShells.x86_64-linux.default = pkgs.mkShellNoCC {
-      shellHook = ''
-        alias vv='${nvim}/bin/nvim'
-        echo 'devshell for testing nvf neovim loaded.'
-      '';
-      packages = [nvim];
-    };
-  };
+      nvim =
+        (inputs.nvf.lib.neovimConfiguration {
+          inherit pkgs;
+          modules = [
+            ./modules/default.nix
+          ];
+        }).neovim;
+    in {
+      apps.default = {
+        type = "app";
+        program = "${nvim}/bin/nvim";
+      };
+      packages.default = nvim;
+      devShells.default = pkgs.mkShellNoCC {
+        shellHook = ''
+          alias vv='${nvim}/bin/nvim'
+          echo 'devshell for testing nvf neovim loaded.'
+        '';
+        packages = [nvim];
+      };
+    });
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nvf.url = "github:notashelf/nvf";
+    flake-utils.url = "github:numtide/flake-utils";
     #nvf.url = "git+file:///home/dragon/opt/nvf";
   };
 }
